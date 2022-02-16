3d27f64c2b167e1705639ee90eab69af2115e61c
Ksenia <ms.ksi2000@mail.ru>
2022-02-16 17:15:31 +0300

    Add Self-sizing Table View Cells

---
+++ AutoLayout-Scroll-task/Cell/ProductCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class ProductCell: UITableViewCell {
+    @IBOutlet var productImage: UIImageView!
+    @IBOutlet var productName: UILabel!
+    @IBOutlet var productDescription: UILabel!
+    @IBOutlet var productPrice: UILabel!
+}
--- AutoLayout-Scroll-task/ViewController.swift
+++ AutoLayout-Scroll-task/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let products = Products.getData()
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 600
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath) as! ProductCell
+        let productItem = products[indexPath.row]
+        cell.productName.text = productItem.name
+        cell.productDescription.text = productItem.description
+        cell.productPrice.text = "$\(productItem.price)"
+        cell.productImage.image = UIImage(named: productItem.image)
+        
+        cell.productName.adjustsFontSizeToFitWidth = false
+        cell.productName.lineBreakMode = .byTruncatingTail
+    
+        cell.productPrice.adjustsFontSizeToFitWidth = true
+        cell.productPrice.lineBreakMode = .byTruncatingTail
+        
+        return cell
+    }
 }
 
